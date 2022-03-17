470660fce44e511bb471ac361979c56492b16d39
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-17 10:51:57 +0100

    using type orm with dotEnv

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,20 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+   /* TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
       entities: [User, Report],
       synchronize: true,
-    }),
+    }),*/
+      ConfigModule.forRoot({
+        isGlobal:true,
+        envFilePath:`.env.${process.env.NODE_ENV}`
+      }),
     UsersModule,
     ReportsModule,
   ],
--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,9 +1,17 @@
 import {Test} from "@nestjs/testing";
 import {AuthService} from "./auth.service";
+import {UsersService} from "./users.service";
 
 it('can read an instance of auth service', async () => {
+    //create a fake copy of users service
+     const fakeUsersService= {
+         find:()=>Promise.resolve([]) ,
+         create:(email:string,password:string)=>Promise.resolve({id:1,email,password})
+     }
+    // @ts-ignore
     const module = await Test.createTestingModule({
-        providers: [AuthService]
+        providers: [AuthService,],
+
     }).compile()
     // copy of authentication service
     const service = module.get(AuthService);
